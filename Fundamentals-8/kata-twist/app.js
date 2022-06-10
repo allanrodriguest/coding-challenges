db445c2639d4e244075ab8ced8772d00344d0bd0
Allan <allanrodriguest@hotmail.com>
2022-06-09 20:46:06 -0400

    add solution

--- Fundamentals-8/kata-twist/app.js
+++ Fundamentals-8/kata-twist/app.js
@@ -4,3 +4,5 @@
 // This is an easy twist to the example kata (provided by Codewars when learning how to create your own kata).
 
 // Add the value "codewars" to the array websites 1,000 times.
+
+const websites = Array.from({ length: 1000 }).fill('codewars')
