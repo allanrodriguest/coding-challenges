d6670026f1c2123132fee8d952381c7566b3637c
Allan <allanrodriguest@hotmail.com>
2022-06-13 22:20:30 -0400

    add solution

--- Fundamentals-8/theres-a-vowel/app.js
+++ Fundamentals-8/theres-a-vowel/app.js
@@ -6,3 +6,14 @@
 // If they are, change the array value to a string of that vowel.
 
 // Return the resulting array.
+
+const vowels = {
+  97: 'a',
+  101: 'e',
+  105: 'i',
+  111: 'o',
+  117: 'u'
+}
+function isVow(arr) {
+  return arr.map(el => (vowels.hasOwnProperty(el) ? vowels[el] : el))
+}
