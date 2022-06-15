8be34b7fd5fa086de963158152b8286dd0c688f4
Allan <allanrodriguest@hotmail.com>
2022-06-14 21:51:58 -0400

    add solution 2

--- Fundamentals-8/js-train-06/app.js
+++ Fundamentals-8/js-train-06/app.js
@@ -4,3 +4,9 @@
 // Coding in function trueOrFalse, function accept 1 parameters:val, try to use the conditional statement if...else, if val value is false (val==false or it can convert to false), should return a string "false", if not, return a string "true".
 
 const trueOrFalse = val => (val ? 'true' : 'false')
+
+// Solution using Boolean obj and toString method
+
+function trueOrFalse(val) {
+  return Boolean(val).toString()
+}
