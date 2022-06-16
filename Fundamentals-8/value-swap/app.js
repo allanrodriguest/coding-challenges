6c3f29a2d3ccfc02b14810ce4b14b8f621ebfec9
Allan <allanrodriguest@hotmail.com>
2022-06-15 21:45:04 -0400

    add solution 2

--- Fundamentals-8/value-swap/app.js
+++ Fundamentals-8/value-swap/app.js
@@ -6,3 +6,7 @@
 // Can you figure out what's wrong here?
 
 const swapValues = args => args.reverse()
+
+// Solution using ES6 Destructuring assignment
+
+const swapValues = arr => ([arr[0], arr[1]] = [arr[1], arr[0]])
