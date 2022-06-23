eff001c506145d0e8283b0a6674783ee5fe0f888
Allan <allanrodriguest@hotmail.com>
2022-06-22 21:30:13 -0400

    add solution

--- Fundamentals-8/smallest-ID/app.js
+++ Fundamentals-8/smallest-ID/app.js
@@ -10,3 +10,13 @@
 // Note: The given array of used IDs may be unsorted. For test reasons there may be duplicate IDs, but you don't have to find or remove them!
 
 // Go on and code some pure awesomeness!
+
+function nextId(ids) {
+  ids = ids
+    .sort((a, b) => a - b)
+    .filter((el, idx) => !idx || el != ids[idx - 1])
+  for (let i = 0; i < ids.length; i++) {
+    if (i !== ids[i]) return i
+  }
+  return ids[ids.length - 1] + 1
+}
