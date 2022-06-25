2dc71025885f6a345a7df9c9e379db1ba74cd95e
Allan <allanrodriguest@hotmail.com>
2022-06-24 20:57:37 -0400

    add solution

--- Fundamentals-8/js-train-10/app.js
+++ Fundamentals-8/js-train-10/app.js
@@ -19,3 +19,16 @@ function pickIt(arr) {
   }
   return [odd, even]
 }
+
+// Simplified Solution
+
+function pickIt(arr) {
+  let odd = []
+  let even = []
+
+  for (var x of arr) {
+    ;(x % 2 ? odd : even).push(x)
+  }
+
+  return [odd, even]
+}
