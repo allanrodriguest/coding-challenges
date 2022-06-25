20086670679eb08f43850291aee66043a7847f32
Allan <allanrodriguest@hotmail.com>
2022-06-24 20:57:06 -0400

    add solution

--- Fundamentals-8/js-train-10/app.js
+++ Fundamentals-8/js-train-10/app.js
@@ -6,3 +6,16 @@
 // I've defined two arrays odd and even in the function, and also wrote the return statement. Your work is to write a for loop.
 
 // If you forgot how to push an element to an array, please refer to lesson 4.
+
+function pickIt(arr) {
+  var odd = [],
+    even = []
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      even.push(arr[i])
+    } else {
+      odd.push(arr[i])
+    }
+  }
+  return [odd, even]
+}
