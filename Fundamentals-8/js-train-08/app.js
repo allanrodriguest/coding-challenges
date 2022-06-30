39401eb77826d017099f7a2b5de4dadf7e5d14a4
Allan <allanrodriguest@hotmail.com>
2022-06-30 18:10:24 -0400

    CC 2022-06-30

---
+++ Fundamentals-8/js-train-08/app.js
@@ -0,0 +1,9 @@
+// 8 kyu
+// Training JS #8: Conditional statement--switch
+
+// In JavaScript, switch can replace multiple if statements.
+
+
+// switch is the keyword and n is the variable to switch. case 1 means when n===1. Following the ":" you can add your code for what to do in that case. The keyword break is used as termination - if you forget break, the code will continue running through the other case statements and default until a break appears. If no case statements match, default is entered.
+
+// In some instances, the switch statement is clearer than the if..else statement.
