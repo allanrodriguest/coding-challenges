8b96b6bd699ebb59f19238e64292293a29f5ca32
Allan <allanrodriguest@hotmail.com>
2022-07-04 20:29:50 -0400

    add solution

--- Fundamentals-8/nearest-sqr-num/app.js
+++ Fundamentals-8/nearest-sqr-num/app.js
@@ -6,3 +6,5 @@
 // For example, if n = 111, then nearest\_sq(n) (nearestSq(n)) equals 121, since 111 is closer to 121, the square of 11, than 100, the square of 10.
 
 // If the n is already the perfect square (e.g. n = 144, n = 81, etc.), you need to just return n.
+
+const nearestSq = n => Math.round(Math.sqrt(n)) ** 2
