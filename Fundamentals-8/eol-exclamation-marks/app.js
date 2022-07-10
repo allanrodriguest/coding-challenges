4746141f1e08390b09eca4851fe6eb9d46422158
Allan <allanrodriguest@hotmail.com>
2022-07-09 20:40:53 -0400

    Coding Challenge Jul 09

---
+++ Fundamentals-8/eol-exclamation-marks/app.js
@@ -0,0 +1,12 @@
+// 8 kyu
+// Exclamation marks series #2: Remove all exclamation marks from the end of sentence
+
+// Remove all exclamation marks from the end of sentence.
+
+// Examples
+// remove("Hi!") === "Hi"
+// remove("Hi!!!") === "Hi"
+// remove("!Hi") === "!Hi"
+// remove("!Hi!") === "!Hi"
+// remove("Hi! Hi!") === "Hi! Hi"
+// remove("Hi") === "Hi"
