f7660266c3412f10737fa7ef06a5a0130f27673a
Allan <allanrodriguest@hotmail.com>
2022-07-10 20:36:26 -0400

    Coding Challenge Jul 10

---
+++ Fundamentals-8/print-arr/app.js
@@ -0,0 +1,10 @@
+// 8 kyu
+// Printing Array elements with Comma delimiters
+
+// Input: Array of elements
+
+// ["h","o","l","a"]
+
+// Output: String with comma delimited elements of the array in th same order.
+
+// "h,o,l,a"
