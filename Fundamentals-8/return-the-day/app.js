f35da44e4c6ef4c8e623604e9f173de6ef962a7c
Allan <allanrodriguest@hotmail.com>
2022-07-12 21:22:24 -0400

    add solution

--- Fundamentals-8/return-the-day/app.js
+++ Fundamentals-8/return-the-day/app.js
@@ -11,3 +11,14 @@
 // 6 returns "Friday"
 // 7 returns "Saturday"
 // Otherwise returns "Wrong, please enter a number between 1 and 7"
+
+const whatday = num =>
+  ({
+    1: 'Sunday',
+    2: 'Monday',
+    3: 'Tuesday',
+    4: 'Wednesday',
+    5: 'Thursday',
+    6: 'Friday',
+    7: 'Saturday'
+  }[num] || `Wrong, please enter a number between 1 and 7`)
