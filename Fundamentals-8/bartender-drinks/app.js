2dd3c48520c329d541faad04d3c6247e594b74a9
Allan <allanrodriguest@hotmail.com>
2022-07-13 21:09:45 -0400

    add solution

--- Fundamentals-8/bartender-drinks/app.js
+++ Fundamentals-8/bartender-drinks/app.js
@@ -14,3 +14,18 @@
 // Note: anything else is the default case: if the input to the function is not any of the values in the table, then the return value should be "Beer".
 
 // Make sure you cover the cases where certain words do not show up with correct capitalization. For example, the input "pOLitiCIaN" should still return "Your tax dollars".
+
+const drinks = {
+  jabroni: 'Patron Tequila',
+  'school counselor': 'Anything with Alcohol',
+  programmer: 'Hipster Craft Beer',
+  'bike gang member': 'Moonshine',
+  politician: 'Your tax dollars',
+  rapper: 'Cristal',
+  beer: 'Beer'
+}
+
+function getDrinkByProfession(param) {
+  let result = param.toLowerCase()
+  return drinks.hasOwnProperty(result) ? drinks[result] : drinks.beer
+}
