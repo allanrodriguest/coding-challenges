e56532d44e0ebfee90cfc1f4f01679401c8d3373
Allan <allanrodriguest@hotmail.com>
2022-07-14 20:55:55 -0400

    add solution

--- Fundamentals-8/js-train-03/app.js
+++ Fundamentals-8/js-train-03/app.js
@@ -13,3 +13,34 @@
 // misson 2: After misson 1 finished. you should click "Attempt" to see my three questions, and write the answer in function answer1, answer2,answer3
 
 // If everything is right, click "Submit" again to submit your code pass this kata.
+
+let a1 = 'A',
+  a2 = 'a',
+  b1 = 'B',
+  b2 = 'b',
+  c1 = 'C',
+  c2 = 'c',
+  d1 = 'D',
+  d2 = 'd',
+  e1 = 'E',
+  e2 = 'e',
+  n1 = 'N',
+  n2 = 'n'
+function Dad() {
+  return d1 + a2 + d2
+}
+function Bee() {
+  return b1 + e2 + e2
+}
+function banana() {
+  return b2 + a2 + n2 + a2 + n2 + a2
+}
+function answer1() {
+  return 'no'
+}
+function answer2() {
+  return 'no'
+}
+function answer3() {
+  return 'yes'
+}
