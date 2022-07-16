02544f2b9034063a6d2feaf79d94aa63e4b11b5b
Allan <allanrodriguest@hotmail.com>
2022-07-15 20:47:28 -0400

    add solution

--- Fundamentals-8/english-speaker/app.js
+++ Fundamentals-8/english-speaker/app.js
@@ -8,3 +8,5 @@
 // Upper or lower case letter does not matter -- "eNglisH" is also correct.
 
 // Return value as boolean values, true for the string to contains "English", false for it does not.
+
+const spEng = sentence => sentence.toLowerCase().includes('english')
