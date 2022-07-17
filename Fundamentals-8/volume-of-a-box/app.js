457fed6dcea8c2d9d06de183b15bfa22cbb99771
Allan <allanrodriguest@hotmail.com>
2022-07-16 20:44:08 -0400

    add solution

--- Fundamentals-8/volume-of-a-box/app.js
+++ Fundamentals-8/volume-of-a-box/app.js
@@ -2,3 +2,10 @@
 // Surface Area and Volume of a Box
 
 // Write a function that returns the total surface area and volume of a box as an array: [area, volume]
+
+function getSize(width, height, depth) {
+  let result = []
+  result.push(2 * (height * width + height * depth + width * depth))
+  result.push(width * height * depth)
+  return result
+}
