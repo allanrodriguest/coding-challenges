512a5d325d447f9be9bb5f954d68db9f6bda9e6d
Allan <allanrodriguest@hotmail.com>
2022-07-27 21:59:59 -0400

    add solution

--- Fundamentals-8/enum-magic-list/app.js
+++ Fundamentals-8/enum-magic-list/app.js
@@ -2,3 +2,5 @@
 // Enumerable Magic - Does My List Include This?
 
 // Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.
+
+const include = (arr, item) => arr.includes(item)
