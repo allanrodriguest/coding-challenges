9c854e5aae63a19369897deaefccfcb0e102514b
Allan <allanrodriguest@hotmail.com>
2022-07-27 22:00:59 -0400

    add solution 2

--- Fundamentals-8/enum-magic-list/app.js
+++ Fundamentals-8/enum-magic-list/app.js
@@ -4,3 +4,9 @@
 // Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.
 
 const include = (arr, item) => arr.includes(item)
+
+// Solution using indexOf
+
+function include(arr, item) {
+  return arr.indexOf(item) !== -1
+}
