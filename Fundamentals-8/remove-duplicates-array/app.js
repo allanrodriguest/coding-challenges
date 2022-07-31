40d4c9384e7cce4419c573671b8973a254f3ce0d
Allan <allanrodriguest@hotmail.com>
2022-07-30 21:08:53 -0400

    add solution

--- Fundamentals-8/remove-duplicates-array/app.js
+++ Fundamentals-8/remove-duplicates-array/app.js
@@ -4,3 +4,14 @@
 // Define a function that removes duplicates from an array of numbers and returns it as a result.
 
 // The order of the sequence has to stay the same.
+
+function distinct(a) {
+  let result = []
+
+  a.forEach(el => {
+    if (!result.includes(el)) {
+      result.push(el)
+    }
+  })
+  return result
+}
