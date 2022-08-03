fd0c24696c3c4f3c60e1ad40711f8af251b1b1c6
Allan <allanrodriguest@hotmail.com>
2022-08-02 21:34:20 -0400

    add solutions

--- Fundamentals-8/grass-function/app.js
+++ Fundamentals-8/grass-function/app.js
@@ -5,3 +5,9 @@
 // A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.
 
 const main = (a, b) => `${a}${b}`
+
+// CO0L solutions
+
+const main = (...a) => a.join('')
+
+const main = String.prototype.concat.bind('')
