0a9ba9cf5bf97c93a1ae5744740cc02756d86eaf
Allan <allanrodriguest@hotmail.com>
2022-08-03 21:23:51 -0400

    add solution

--- Fundamentals-8/array-madness/app.js
+++ Fundamentals-8/array-madness/app.js
@@ -9,3 +9,7 @@
 
 // arrayMadness([4, 5, 6], [1, 2, 3]); // returns true since 4 ** 2 + 5 ** 2 + 6 ** 2 > 1 ** 3 + 2 ** 3 + 3 ** 3
 // Get your timer out. Are you ready? Ready, get set, GO!!!
+
+const arrayMadness = (a, b) =>
+  [...a].map(el => el ** 2).reduce((acc, c) => acc + c, 0) >
+  [...b].map(el => el ** 3).reduce((acc, c) => acc + c, 0)
