821723920a5948ca6407ad6023cc89766b07afd0
Allan <allanrodriguest@hotmail.com>
2022-08-04 21:16:35 -0400

    add solution

--- Fundamentals-8/str-clean/app.js
+++ Fundamentals-8/str-clean/app.js
@@ -8,3 +8,10 @@
 // '123456789'           -> ''
 // 'This looks5 grea8t!' -> 'This looks great!'
 // Your harried co-workers are looking to you for a solution to take this garbled text and remove all of the numbers. Your program will take in a string and clean out all numeric characters, and return a string with spacing and special characters ~#$%^&!@*():;"'.,? all intact.
+
+function stringClean(s) {
+  return s
+    .split('')
+    .filter(n => n != parseInt(n))
+    .join('')
+}
