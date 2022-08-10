8765f99678ca0b9fc8f4da850ca1a472b5ebc63c
Allan <allanrodriguest@hotmail.com>
2022-08-09 20:32:38 -0400

    add solution

--- Fundamentals-8/determine-the-gender/app.js
+++ Fundamentals-8/determine-the-gender/app.js
@@ -7,3 +7,8 @@
 // Determine if the sex of the offspring will be male or female based on the X or Y chromosome present in the male's sperm.
 
 // If the sperm contains the X chromosome, return "Congratulations! You're going to have a daughter."; If the sperm contains the Y chromosome, return "Congratulations! You're going to have a son.";
+
+const chromosomeCheck = sperm =>
+  `Congratulations! You're going to have a ${
+    sperm === 'XY' ? 'son' : 'daughter'
+  }.`
