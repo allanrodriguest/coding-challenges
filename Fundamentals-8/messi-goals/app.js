ec51162d5daeb562cd277096986dd6b0a91989db
Allan <allanrodriguest@hotmail.com>
2022-08-15 18:08:39 -0400

    add solution

--- Fundamentals-8/messi-goals/app.js
+++ Fundamentals-8/messi-goals/app.js
@@ -17,3 +17,9 @@
 // championsLeagueGoals
 // copaDelReyGoals
 // Create a fourth variable named totalGoals that stores the sum of all of Messi's goals for this year.
+
+const laLigaGoals = 43,
+  championsLeagueGoals = 10,
+  copaDelReyGoals = 5
+let goals = [43, 10, 5]
+let totalGoals = goals.reduce((acc, c) => acc + c, 0)
