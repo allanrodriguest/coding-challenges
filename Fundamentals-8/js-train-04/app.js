641bdf5020a11415a6417b5b49b5a7aab5b5778e
Allan <allanrodriguest@hotmail.com>
2022-08-16 18:02:23 -0400

    add solution

--- Fundamentals-8/js-train-04/app.js
+++ Fundamentals-8/js-train-04/app.js
@@ -9,3 +9,16 @@
 // index always start from 0, so the first element of array is arr[0], the last element of array is arr[arr.length-1].
 
 // If we want to add new elements to the array, you can use the array method: push(). It can add an element to the end of the array. Instead, if we want to remove the last element of the array, you can use the array method: pop(). for example:
+
+const getLength = arr => arr.length
+const getFirst = arr => arr[0]
+const getLast = arr => arr[arr.length - 1]
+function pushElement(arr) {
+  var el = 1
+  arr.push(el)
+  return arr
+}
+function popElement(arr) {
+  arr.pop()
+  return arr
+}
