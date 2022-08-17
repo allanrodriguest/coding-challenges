762d6f06ce32c89d630ef6a68a2baa4eedce07ca
Allan <allanrodriguest@hotmail.com>
2022-08-17 17:53:57 -0400

    Coding Challenge Aug 17

---
+++ Fundamentals-8/template-str-5/app.js
@@ -0,0 +1,4 @@
+// 8 kyu
+// String Templates - Bug Fixing #5
+
+// Oh no! Timmy hasn't followed instructions very carefully and forgot how to use the new String Template feature, Help Timmy with his string template so it works as he expects!
