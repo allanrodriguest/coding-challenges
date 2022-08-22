d6bf85bd9d14687bf134e9afd97720f3f139033c
Allan <allanrodriguest@hotmail.com>
2022-08-22 12:40:30 -0400

    add solution 2

--- Fundamentals-8/bin-to-decimal/app.js
+++ Fundamentals-8/bin-to-decimal/app.js
@@ -4,3 +4,8 @@
 // Complete the function which converts a binary number (given as a string) to a decimal number.
 
 const binToDec = bin => parseInt(bin, 2)
+
+
+// Solution using ES6 trick
+
+const binToDec = s => '0b' + s - 0
