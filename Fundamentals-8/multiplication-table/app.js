cc47f835b6107dc23a27463839ade738be253f0c
Allan <allanrodriguest@hotmail.com>
2022-08-23 21:07:13 -0400

    add solution

--- Fundamentals-8/multiplication-table/app.js
+++ Fundamentals-8/multiplication-table/app.js
@@ -18,3 +18,9 @@
 // P. S. You can use \n in string to jump to the next line.
 
 // Note: newlines should be added between rows, but there should be no trailing newline at the end. If you're unsure about the format, look at the sample tests.
+
+function multiTable(number) {
+  return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    .map(item => `${item} * ${number} = ${item * number}`)
+    .join('\n')
+}
