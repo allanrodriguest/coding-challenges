009e80196f2bfd2e4d08689cb4a120a054b2bd98
Allan <allanrodriguest@hotmail.com>
2022-08-24 20:52:56 -0400

    add solution 2

--- Fundamentals-8/wolf-sheep/app.js
+++ Fundamentals-8/wolf-sheep/app.js
@@ -26,3 +26,12 @@ function warnTheSheep(queue) {
         queue.length - wolf - 1
       }! You are about to be eaten by a wolf!`
 }
+
+// using reverse() makes it easier to find the solution
+
+function warnTheSheep(queue) {
+  const position = queue.reverse().indexOf('wolf')
+  return position === 0
+    ? 'Pls go away and stop eating my sheep'
+    : `Oi! Sheep number ${position}! You are about to be eaten by a wolf!`
+}
