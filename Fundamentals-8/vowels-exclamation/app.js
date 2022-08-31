899b5c113b22524846d2827c492a8ed8a6a8af8e
Allan <allanrodriguest@hotmail.com>
2022-08-30 21:27:28 -0400

    Coding Challenge August 30

---
+++ Fundamentals-8/vowels-exclamation/app.js
@@ -0,0 +1,10 @@
+// 8 kyu
+// Exclamation marks series #11: Replace all vowel to exclamation mark in the sentence
+
+// Replace all vowel to exclamation mark in the sentence. aeiouAEIOU is vowel.
+
+// Examples
+// replace("Hi!") === "H!!"
+// replace("!Hi! Hi!") === "!H!! H!!"
+// replace("aeiou") === "!!!!!"
+// replace("ABCDE") === "!BCD!"
