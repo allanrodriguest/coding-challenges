67f27d136034796efe004d758bd57b0e33b92c8d
Allan <allanrodriguest@hotmail.com>
2022-09-01 21:09:31 -0400

    Coding Challenge Sept 01

---
+++ Fundamentals-8/ASCII-value/app.js
@@ -0,0 +1,11 @@
+// 8 kyu
+// get character from ASCII Value
+
+// Write a function get_char() / getChar() which takes a number and returns the corresponding ASCII char for that value.
+
+// Example:
+
+// get_char(65)
+// should return:
+
+// 'A'
