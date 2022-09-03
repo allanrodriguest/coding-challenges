ee427e5e43708e0906ffb340dfb5d4a6a0d9fa21
Allan <allanrodriguest@hotmail.com>
2022-09-02 21:07:48 -0400

    add solution 2

--- Fundamentals-8/101-dalmatians/app.js
+++ Fundamentals-8/101-dalmatians/app.js
@@ -26,3 +26,21 @@ function howManyDalmatians(number) {
       : dogs[2]
   return respond
 }
+
+// Refactor 
+
+const dogs = [
+  'Hardly any',
+  'More than a handful!',
+  "Woah that's a lot of dogs!",
+  '101 DALMATIANS!!!'
+]
+
+const howManyDalmatians = number =>
+  number <= 10
+    ? dogs[0]
+    : number <= 50
+    ? dogs[1]
+    : number == 101
+    ? dogs[3]
+    : dogs[2]
