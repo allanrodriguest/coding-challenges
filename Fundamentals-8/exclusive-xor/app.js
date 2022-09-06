8a486cc337821a7557f0f8818721e64e1e4d0dca
Allan <allanrodriguest@hotmail.com>
2022-09-05 20:50:55 -0400

    add solution 2

--- Fundamentals-8/exclusive-xor/app.js
+++ Fundamentals-8/exclusive-xor/app.js
@@ -10,3 +10,7 @@
 // Since we cannot define keywords in Javascript (well, at least I don't know how to do it), your task is to define a function xor(a, b) where a and b are the two expressions to be evaluated. Your xor function should have the behaviour described above, returning true if exactly one of the two expressions evaluate to true, false otherwise.
 
 const xor = (a, b) => a !== b
+
+// 15% faster solution using bitwise
+
+const xor = (a, b) => !!(a ^ b)
