572b81dac7a5c30d86adb6c1717dc2e3d3afd3cc
Allan <allanrodriguest@hotmail.com>
2022-09-05 20:48:22 -0400

    add solution

--- Fundamentals-8/exclusive-xor/app.js
+++ Fundamentals-8/exclusive-xor/app.js
@@ -8,3 +8,5 @@
 // true xor true == false // Both are true.  "xor" only returns true if EXACTLY one of the two expressions evaluate to true.
 // Task
 // Since we cannot define keywords in Javascript (well, at least I don't know how to do it), your task is to define a function xor(a, b) where a and b are the two expressions to be evaluated. Your xor function should have the behaviour described above, returning true if exactly one of the two expressions evaluate to true, false otherwise.
+
+const xor = (a, b) => a !== b
