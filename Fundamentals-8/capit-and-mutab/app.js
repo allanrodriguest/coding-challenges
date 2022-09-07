f8464f1c18fb1b59927cbd13ff7e57cb494665fb
Allan <allanrodriguest@hotmail.com>
2022-09-06 20:40:33 -0400

    add solution

--- Fundamentals-8/capit-and-mutab/app.js
+++ Fundamentals-8/capit-and-mutab/app.js
@@ -6,3 +6,5 @@
 // Unfortunately, they have now left and the code they gave you doesn't work. Fix the helper function they wrote so that it works as intended (i.e. make the first character in the string "word" upper case).
 
 // Don't worry about numbers, special characters, or non-string types being passed to the function. The string lengths will be from 1 character up to 10 characters, but will never be empty.
+
+const capitalizeWord = word => word[0].toUpperCase() + word.slice(1)
