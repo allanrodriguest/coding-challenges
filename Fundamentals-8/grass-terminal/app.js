e577a2e7396dad80842fdcdb75ec9058edc3bbf1
Allan <allanrodriguest@hotmail.com>
2022-09-12 20:56:58 -0400

    add solution 2

--- Fundamentals-8/grass-terminal/app.js
+++ Fundamentals-8/grass-terminal/app.js
@@ -4,3 +4,7 @@
 // Create a combat function that takes the player's current health and the amount of damage recieved, and returns the player's new health. Health can't be less than 0.
 
 const combat = (health, damage) => (health - damage > 0 ? health - damage : 0)
+
+// Solution using Math.max()
+
+const combat = (health, damage) => Math.max(0, health - damage)
