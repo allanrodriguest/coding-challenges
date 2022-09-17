cecf6feae22afe9071c3df60baa3fe0c900c7c9f
Allan <allanrodriguest@hotmail.com>
2022-09-16 20:28:08 -0400

    add solution

--- Fundamentals-8/vowel-remover/app.js
+++ Fundamentals-8/vowel-remover/app.js
@@ -10,3 +10,11 @@
 // "HELLO"     -->  "HELLO"
 // don't worry about uppercase vowels
 // y is not considered a vowel for this kata
+
+const shortcut = str => {
+  const vowels = ['a', 'e', 'i', 'o', 'u']
+  return str
+    .split('')
+    .filter(item => !vowels.includes(item))
+    .join('')
+}
