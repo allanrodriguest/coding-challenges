4f2b2126f567cfbcfea58202e81e1a646dc32e64
Allan <allanrodriguest@hotmail.com>
2022-09-18 20:10:39 -0400

    Coding Challenge Sept 18

---
+++ Fundamentals-8/remove-excl-mark/app.js
@@ -0,0 +1,12 @@
+// 8 kyu
+// Exclamation marks series #1: Remove an exclamation mark from the end of string
+
+// Remove an exclamation mark from the end of a string. For a beginner kata, you can assume that the input data is always a string, no need to verify it.
+
+// Examples
+// remove("Hi!") == "Hi"
+// remove("Hi!!!") == "Hi!!"
+// remove("!Hi") == "!Hi"
+// remove("!Hi!") == "!Hi"
+// remove("Hi! Hi!") == "Hi! Hi"
+// remove("Hi") == "Hi"
