719a6f90dd0400f594adcf284b347594b8c5982b
Allan <allanrodriguest@hotmail.com>
2022-09-23 20:55:31 -0400

    add solution

--- 7kyu/vowel-count/app.js
+++ 7kyu/vowel-count/app.js
@@ -5,3 +5,8 @@
 // We will consider a, e, i, o, u as vowels for this Kata (but not y).
 
 // The input string will only consist of lower case letters and/or spaces.
+
+function getCount(str) {
+  const vowels = ['a', 'e', 'i', 'o', 'u']
+  return str.split('').filter(item => vowels.includes(item)).length
+}
