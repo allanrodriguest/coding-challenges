4f6e078a791de5b5722c051569047ac7819da0a9
Allan <allanrodriguest@hotmail.com>
2022-09-27 20:20:13 -0400

    add solution 2

--- Fundamentals-8/square-it/app.js
+++ Fundamentals-8/square-it/app.js
@@ -5,3 +5,9 @@
 // Now you have to write a function that takes an argument and returns the square of it.
 
 const square = n => n ** 2
+
+// Solution using Math.pow()
+
+function square(num){
+  return Math.pow(num, 2);
+};
