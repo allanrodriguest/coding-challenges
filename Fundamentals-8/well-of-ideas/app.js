f7bb3801797558f32f32672e79a598167d78de13
Allan <allanrodriguest@hotmail.com>
2022-10-01 22:05:54 -0400

    add solution

--- Fundamentals-8/well-of-ideas/app.js
+++ Fundamentals-8/well-of-ideas/app.js
@@ -5,3 +5,13 @@
 // For every good kata idea there seem to be quite a few bad ones!
 
 // In this kata you need to check the provided array (x) for good ideas 'good' and bad ideas 'bad'. If there are one or two good ideas, return 'Publish!', if there are more than 2 return 'I smell a series!'. If there are no good ideas, as is often the case, return 'Fail!'.
+
+function well(x) {
+  return x.filter(item => item === 'good').length > 2
+    ? 'I smell a series!'
+    : x.filter(item => item === 'good').length === 0
+    ? 'Fail!'
+    : 'Publish!'
+}
+
+// 
