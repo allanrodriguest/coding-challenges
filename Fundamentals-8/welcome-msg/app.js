293583f4b1972fb25dca90bf278d70f2e3abbf12
Allan <allanrodriguest@hotmail.com>
2022-10-02 21:56:15 -0400

    add solution 2

--- Fundamentals-8/welcome-msg/app.js
+++ Fundamentals-8/welcome-msg/app.js
@@ -33,3 +33,46 @@ function greet(language) {
     ? greetings[language]
     : greetings.english
 }
+
+// Solution using switch statement
+
+function greet(language) {
+  switch (language) {
+    case 'english':
+      return 'Welcome'
+    case 'czech':
+      return 'Vitejte'
+    case 'danish':
+      return 'Velkomst'
+    case 'dutch':
+      return 'Welkom'
+    case 'estonian':
+      return 'Tere tulemast'
+    case 'finnish':
+      return 'Tervetuloa'
+    case 'flemish':
+      return 'Welgekomen'
+    case 'french':
+      return 'Bienvenue'
+    case 'german':
+      return 'Willkommen'
+    case 'irish':
+      return 'Failte'
+    case 'italian':
+      return 'Benvenuto'
+    case 'latvian':
+      return 'Gaidits'
+    case 'lithuanian':
+      return 'Laukiamas'
+    case 'polish':
+      return 'Witamy'
+    case 'spanish':
+      return 'Bienvenido'
+    case 'swedish':
+      return 'Valkommen'
+    case 'welsh':
+      return 'Croeso'
+    default:
+      return 'Welcome'
+  }
+}
