bdf25eb13cd2d61c6e1efd357afefd7002dfde35
Allan <allanrodriguest@hotmail.com>
2022-10-04 21:27:14 -0400

    add solution

--- Fundamentals-8/nth-power/app.js
+++ Fundamentals-8/nth-power/app.js
@@ -10,3 +10,5 @@
 
 // array = [1, 2, 3, 4] and N = 2, then the result is 3^2 == 9;
 // array = [1, 2, 3] and N = 3, but N is outside of the array, so the result is -1.
+
+const index = (arr, n) => Math.pow(arr[n], n) || -1
