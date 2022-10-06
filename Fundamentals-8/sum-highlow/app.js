d1a51ffb57058fea20d02e4320eb11506ec7192a
Allan <allanrodriguest@hotmail.com>
2022-10-05 20:34:52 -0400

    add solution

--- Fundamentals-8/sum-highlow/app.js
+++ Fundamentals-8/sum-highlow/app.js
@@ -14,3 +14,10 @@
 // { 1, 1, 11, 2, 3 } => 6
 // Input validation
 // If an empty value ( null, None, Nothing etc. ) is given instead of an array, or the given array is an empty list or a list with only 1 element, return 0.
+
+function sumArray(array) {
+  if (!array || array.length < 3) return 0
+  array.sort((a, b) => a - b).pop()
+  array.shift()
+  return array.reduce((acc, c) => acc + c, 0)
+}
