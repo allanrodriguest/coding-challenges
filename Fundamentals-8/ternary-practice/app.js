74bd6b98bb6280f5371ce242b9f7a7a54e3cebab
Allan <allanrodriguest@hotmail.com>
2022-10-07 20:14:56 -0400

    add solution

--- Fundamentals-8/ternary-practice/app.js
+++ Fundamentals-8/ternary-practice/app.js
@@ -6,3 +6,7 @@
 // n >= 5 and n < 10	95
 // n >= 10	90
 // You can use if..else or ternary operator to complete it.
+
+function saleHotdogs(n) {
+  return n < 5 ? n * 100 : n >= 5 && n < 10 ? n * 95 : n * 90
+}
