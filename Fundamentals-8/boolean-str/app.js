33141c71f8ffd7431d46cfe3a2d72d13631fdc06
Allan <allanrodriguest@hotmail.com>
2022-10-15 21:02:52 -0400

    Add solution

--- Fundamentals-8/boolean-str/app.js
+++ Fundamentals-8/boolean-str/app.js
@@ -1,2 +1,15 @@
 // Create a method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
 
+
+// First solution
+
+function boolToWord(bool) {
+  return bool ? 'Yes' : 'No'
+}
+
+// Fancy solution
+
+function boolToWord(bool) {
+  return ['No', 'Yes'][+bool]
+}
+
