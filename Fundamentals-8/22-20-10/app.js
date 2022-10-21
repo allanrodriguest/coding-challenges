60e3d1869d025d56bb5c8b64b5d52e816ecdd222
Allan <allanrodriguest@hotmail.com>
2022-10-20 20:53:26 -0400

    Add solution

--- Fundamentals-8/22-20-10/app.js
+++ Fundamentals-8/22-20-10/app.js
@@ -4,3 +4,12 @@
 
 // First Solution
 
+function lovefunc(flower1, flower2) {
+  return (flower1 % 2 === 0 && flower2 % 2 !== 0) ||(flower1 % 2 !== 0 && flower2 % 2 === 0)
+}
+
+// Better Solution
+
+function lovefunc(flower1, flower2) {
+  return flower1 % 2 !== flower2 % 2
+}
