b4638c56b072eb661467152b580d8d7a6c4ba6dc
Allan <allanrodriguest@hotmail.com>
2022-10-26 20:49:34 -0400

    Add Solution

--- Fundamentals-8/reverse-words/app.js
+++ Fundamentals-8/reverse-words/app.js
@@ -8,3 +8,5 @@
 // Example(Input --> Output):
 
 // "The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The"
+
+const reverseWords = str => str.split(' ').reverse().join(' ')
