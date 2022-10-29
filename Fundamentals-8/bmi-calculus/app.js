93d16152c101f3655431b4ca50adbb490f8737aa
Allan <allanrodriguest@hotmail.com>
2022-10-28 21:07:33 -0400

    Add Solution

--- Fundamentals-8/bmi-calculus/app.js
+++ Fundamentals-8/bmi-calculus/app.js
@@ -10,3 +10,11 @@
 // if bmi <= 30.0 return "Overweight"
 
 // if bmi > 30 return "Obese"
+
+function bmi(weight, height) {
+  let bmIndex = weight / Math.pow(height, 2)
+  if (bmIndex <= 18.5) return 'Underweight'
+  else if (bmIndex <= 25) return 'Normal'
+  else if (bmIndex <= 30) return 'Overweight'
+  else return 'Obese'
+}
