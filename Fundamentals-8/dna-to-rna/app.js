a638c4dc0229a33cff9d91dc949c8463a5e19834
Allan <allanrodriguest@hotmail.com>
2022-10-31 20:23:43 -0400

    Add Solution

--- Fundamentals-8/dna-to-rna/app.js
+++ Fundamentals-8/dna-to-rna/app.js
@@ -11,3 +11,10 @@
 
 // "GCAT"  =>  "GCAU"
 // The input string can be of arbitrary length - in particular, it may be empty. All input is guaranteed to be valid, i.e. each input string will only ever consist of 'G', 'C', 'A' and/or 'T'.
+
+function DNAtoRNA(dna) {
+  return dna
+    .split('')
+    .map(item => (item === 'T' ? (item = 'U') : item))
+    .join('')
+}
