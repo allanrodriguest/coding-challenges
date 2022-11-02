42c70f85b0b6fe68777f65713d28495a8b972f94
Allan <allanrodriguest@hotmail.com>
2022-11-02 18:03:54 -0400

    Add Solution

--- Fundamentals-8/odds-below-n/app.js
+++ Fundamentals-8/odds-below-n/app.js
@@ -6,3 +6,10 @@
 // Examples (Input -> Output)
 // 7  -> 3 (because odd numbers below 7 are [1, 3, 5])
 // 15 -> 7 (because odd numbers below 15 are [1, 3, 5, 7, 9, 11, 13])
+
+function oddCount(n) {
+  return Array.from({ length: n }, (v, i) => i).filter(
+    item => !(item % 2 === 0)
+  ).length
+}
+
