c6bdfca26f28b21f2562eef91ac3f8e0acacc452
Allan <allanrodriguest@hotmail.com>
2022-11-02 21:58:11 -0400

    CC 2022-11-02

--- eloquentJS-chapter06/problem-set01.js
+++ eloquentJS-chapter06/problem-set01.js
@@ -8,6 +8,22 @@
 
 // // Your code here.
 
+class Vec {
+  constructor(x,y){
+    this.x = x;
+    this.y = y;
+  }
+  plus(target){
+    return new Vec(this.x + target.x, this.y + target.y);
+  }
+  minus(target){
+    return new Vec(this.x - target.y, this.y - target.y);
+  }
+  get length() {
+    return Math.sqrt(this.x**2 + this.y**2)
+  }
+}
+
 console.log(new Vec(1, 2).plus(new Vec(2, 3)));
 // → Vec{x: 3, y: 5}
 console.log(new Vec(1, 2).minus(new Vec(2, 3)));
