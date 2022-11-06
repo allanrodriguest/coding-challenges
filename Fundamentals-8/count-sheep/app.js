0007df77ceb418dc5bce5dcff8294bc1953646e6
Allan <allanrodriguest@hotmail.com>
2022-11-05 20:09:15 -0500

    Add Solution

--- Fundamentals-8/count-sheep/app.js
+++ Fundamentals-8/count-sheep/app.js
@@ -5,3 +5,15 @@
 
 // Task:
 // Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+
+var countSheep = function (num) {
+  if (num.length === 0) return ''
+  let i = 1
+  let result = ''
+  while (num > 0) {
+    result += `${i} sheep...`
+    i++
+    num--
+  }
+  return result
+}
