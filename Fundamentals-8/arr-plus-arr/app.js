3fa1d4f9d741bfc2d704112db7bf0bd867ec5b2f
Allan <allanrodriguest@hotmail.com>
2022-11-08 19:53:17 -0500

    Add Other Solutions

--- Fundamentals-8/arr-plus-arr/app.js
+++ Fundamentals-8/arr-plus-arr/app.js
@@ -10,3 +10,15 @@ function arrayPlusArray(arr1, arr2) {
     arr1.reduce((acc, c) => acc + c, 0) + arr2.reduce((acc, c) => acc + c, 0)
   )
 }
+
+// Solution using spread operator
+
+function arrayPlusArray(arr1, arr2) {
+  return [...arr1, ...arr2].reduce((a, b) => a + b)
+}
+
+// Solution using concat before reduce
+
+function arrayPlusArray(arr1, arr2) {
+  return arr1.concat(arr2).reduce((acc, cur) => acc + cur)
+}
