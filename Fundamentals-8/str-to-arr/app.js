adca9e95547ff46f80a866c8dfafbd88e764d725
Allan <allanrodriguest@hotmail.com>
2022-11-09 19:58:28 -0500

    Add Solution

--- Fundamentals-8/str-to-arr/app.js
+++ Fundamentals-8/str-to-arr/app.js
@@ -8,3 +8,8 @@
 
 // "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
 
+function arrayPlusArray(arr1, arr2) {
+  return (
+    arr1.reduce((acc, c) => acc + c, 0) + arr2.reduce((acc, c) => acc + c, 0)
+  )
+}
