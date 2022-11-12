75070aed55fdf96676fe2b35271b2ba648de8fb0
Allan <allanrodriguest@hotmail.com>
2022-11-11 20:13:00 -0500

    Solution using Bitwise

--- Fundamentals-8/double-the-int/app.js
+++ Fundamentals-8/double-the-int/app.js
@@ -4,3 +4,9 @@
 // Code as fast as you can! You need to double the integer and return it.
 
 const doubleInteger = _ => _ * 2
+
+// Solution using Bitwise <<
+
+function doubleInteger(i) {
+  return i << 1
+}
