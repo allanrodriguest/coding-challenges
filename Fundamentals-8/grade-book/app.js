86490ce181d22df1090590bafb639e2e9fc7f1b9
Allan <allanrodriguest@hotmail.com>
2022-11-15 20:18:34 -0500

    Add Solution

--- Fundamentals-8/grade-book/app.js
+++ Fundamentals-8/grade-book/app.js
@@ -12,3 +12,11 @@
 // 60 <= score < 70	'D'
 // 0 <= score < 60	'F'
 // Tested values are all between 0 and 100. Theres is no need to check for negative values or values greater than 100.
+function getGrade(s1, s2, s3) {
+  let answer = (s1 + s2 + s3) / 3
+  if (answer < 60) return 'F'
+  else if (answer < 70) return 'D'
+  else if (answer < 80) return 'C'
+  else if (answer < 90) return 'B'
+  else return 'A'
+}
