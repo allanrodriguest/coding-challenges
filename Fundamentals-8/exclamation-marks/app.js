8d0d34f1ec7611964f65ae2cba5644a7ce9b9432
Allan <allanrodriguest@hotmail.com>
2022-11-17 20:33:07 -0500

    Add Solution

--- Fundamentals-8/exclamation-marks/app.js
+++ Fundamentals-8/exclamation-marks/app.js
@@ -3,3 +3,7 @@
 
 // DESCRIPTION:
 // Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+function removeExclamationMarks(s) {
+  return s.replaceAll('!', '')
+}
