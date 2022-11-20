b6e462b9914c22bff5eb9353f36ab17359c02756
Allan <allanrodriguest@hotmail.com>
2022-11-19 20:50:04 -0500

    Add Solution

--- Fundamentals-8/car-rental-calc/app.js
+++ Fundamentals-8/car-rental-calc/app.js
@@ -9,3 +9,10 @@
 // Every day you rent the car costs $40. If you rent the car for 7 or more days, you get $50 off your total. Alternatively, if you rent the car for 3 or more days, you get $20 off your total.
 
 // Write a code that gives out the total amount for different days(d).
+
+function rentalCarCost(d) {
+  const DAILY_FEE = 40
+  if (d >= 7) return d * DAILY_FEE - 50
+  else if (d >= 3) return d * DAILY_FEE - 20
+  else return d * DAILY_FEE
+}
