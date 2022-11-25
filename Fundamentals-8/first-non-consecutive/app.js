f6993522d03eddba68e81370f93642d2b3217fc6
Allan <allanrodriguest@hotmail.com>
2022-11-24 22:38:25 -0500

    Clever Solution

--- Fundamentals-8/first-non-consecutive/app.js
+++ Fundamentals-8/first-non-consecutive/app.js
@@ -29,3 +29,11 @@ function firstNonConsecutive(arr) {
   }
   return null
 }
+
+// Clever Solution
+
+function firstNonConsecutive(arr) {
+  let result = arr.find((val, index) => val !== index + arr[0])
+
+  return Number.isInteger(result) ? result : null
+}
