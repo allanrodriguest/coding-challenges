84a940d28ebcfbf6474a045677d06eee3d7d1cbb
Allan <allanrodriguest@hotmail.com>
2022-11-27 20:31:32 -0500

    Add Solution

--- Fundamentals-8/will-it-fit/app.js
+++ Fundamentals-8/will-it-fit/app.js
@@ -16,3 +16,5 @@
 // Usage Examples:
 // cap = 10, on = 5, wait = 5 --> 0 # He can fit all 5 passengers
 // cap = 100, on = 60, wait = 50 --> 10 # He can't fit 10 of the 50 waiting
+
+const enough = (cap, on, wait) => (cap > on + wait ? 0 : on + wait - cap)
