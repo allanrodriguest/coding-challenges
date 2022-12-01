b9e63a1267f3f2057b9bf1b1360718aebc38dfc8
Allan <allanrodriguest@hotmail.com>
2022-11-30 19:32:48 -0500

    Add Solution

--- Fundamentals-8/get-by-id/app.js
+++ Fundamentals-8/get-by-id/app.js
@@ -8,3 +8,24 @@
 // 3 --> "Earth"
 
 // P: Input integer, return a string with the corresponding planet
+
+function getPlanetName(id) {
+  switch (id) {
+    case 1:
+      return 'Mercury'
+    case 2:
+      return 'Venus'
+    case 3:
+      return 'Earth'
+    case 4:
+      return 'Mars'
+    case 5:
+      return 'Jupiter'
+    case 6:
+      return 'Saturn'
+    case 7:
+      return 'Uranus'
+    case 8:
+      return 'Neptune'
+  }
+}
