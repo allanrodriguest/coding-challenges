d78b707ac7c2db0ff4014edbfb1c4ec4b924172d
Allan <allanrodriguest@hotmail.com>
2022-11-30 19:28:47 -0500

    CC 2022-11-30

---
+++ Fundamentals-8/get-by-id/app.js
@@ -0,0 +1,10 @@
+// 8 kyu
+// Get Planet Name By ID
+// DESCRIPTION:
+// The function is not returning the correct values. Can you figure out why?
+
+// Example (Input --> Output ):
+
+// 3 --> "Earth"
+
+// P: Input integer, return a string with the corresponding planet
