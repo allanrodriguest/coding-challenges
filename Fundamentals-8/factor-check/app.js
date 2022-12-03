d60b430cfb19038089e6151e3a68552cc8ab25a8
Allan <allanrodriguest@hotmail.com>
2022-12-02 19:42:59 -0500

    Add Solution

--- Fundamentals-8/factor-check/app.js
+++ Fundamentals-8/factor-check/app.js
@@ -16,3 +16,5 @@
 // For example 2 is not a factor of 7 because: 7 % 2 = 1
 
 // Note: base is a non-negative number, factor is a positive number.
+
+const checkForFactor = (base, factor) => base % factor === 0
