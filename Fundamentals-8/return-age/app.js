31f6580a350f1b50d9a023b1e9a98707acc909b8
Allan <allanrodriguest@hotmail.com>
2022-12-04 20:01:18 -0500

    parseInt solution

--- Fundamentals-8/return-age/app.js
+++ Fundamentals-8/return-age/app.js
@@ -9,3 +9,7 @@
 // Assume the test input string is always a valid string. For example, the test input may be "1 year old" or "5 years old". The first character in the string is always a number.
 
 const getAge = str => +str[0]
+
+// parseInt the whole argument works if the string starts with a number
+
+const getAge = parseInt
