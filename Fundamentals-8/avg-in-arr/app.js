85cf698f63b0f634d7049ef874c2942d5a585ba3
Allan <allanrodriguest@hotmail.com>
2022-12-13 18:46:36 -0500

    Add Solution

--- Fundamentals-8/avg-in-arr/app.js
+++ Fundamentals-8/avg-in-arr/app.js
@@ -4,3 +4,7 @@
 // Write a function which calculates the average of the numbers in a given list.
 
 // Note: Empty arrays should return 0.
+
+function findAverage(array) {
+  return array.reduce((acc, sum) => acc + sum, 0) / array.length || 0
+}
