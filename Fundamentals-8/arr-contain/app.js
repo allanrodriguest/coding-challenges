a774579c5dc0e74d4d1effebd78c5e3aca50a146
Allan <allanrodriguest@hotmail.com>
2022-12-14 18:39:26 -0500

    Add Solution

--- Fundamentals-8/arr-contain/app.js
+++ Fundamentals-8/arr-contain/app.js
@@ -6,3 +6,7 @@
 // Array can contain numbers or strings. X can be either.
 
 // Return true if the array contains the value, false if not.
+
+function check(arr, item) {
+  return arr.includes(item)
+}
