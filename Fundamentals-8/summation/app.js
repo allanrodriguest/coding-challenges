96882abb624bd3591e37c8193744719d5a00ee7a
Allan <allanrodriguest@hotmail.com>
2022-12-24 16:17:11 -0500

    Add Solution

--- Fundamentals-8/summation/app.js
+++ Fundamentals-8/summation/app.js
@@ -6,3 +6,11 @@
 
 // 2 -> 3 (1 + 2)
 // 8 -> 36 (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8)
+
+var summation = function (num) {
+  let sum = 0
+  for (let i = 1; i <= num; i++) {
+    sum += i
+  }
+  return sum
+}
