c1a94a2220bcd8e8327df384e84029dce6b90803
Allan <allanrodriguest@hotmail.com>
2022-12-26 21:07:08 -0500

    Solution using Math.pow

--- Fundamentals-8/powers-of-two/app.js
+++ Fundamentals-8/powers-of-two/app.js
@@ -22,3 +22,13 @@ function powersOfTwo(n) {
 function powersOfTwo(n) {
   return Array.from({ length: n + 1 }, (v, k) => 2 ** k)
 }
+
+// Math.pow most obvious solution
+
+function powersOfTwo(n) {
+  var result = []
+  for (var i = 0; i <= n; i++) {
+    result.push(Math.pow(2, i))
+  }
+  return result
+}
