75091a31043787483f2978795708412e95fbaf3c
Allan <allanrodriguest@hotmail.com>
2023-01-06 17:51:34 -0500

    Add Solution

--- Fundamentals-8/n-divisible-by-xy/app.js
+++ Fundamentals-8/n-divisible-by-xy/app.js
@@ -8,3 +8,5 @@
 // 2) n =  12, x = 2, y = 6 =>  true because  12 is divisible by 2 and 6
 // 3) n = 100, x = 5, y = 3 => false because 100 is not divisible by 3
 // 4) n =  12, x = 7, y = 5 => false because  12 is neither divisible by 7 nor 5
+
+const isDivisible = (n, x, y) => n % x == 0 && n % y == 0
