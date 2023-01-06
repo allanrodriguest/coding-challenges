b81aa05beb70cc48eec3a0b988e15c04b900cc14
Allan <allanrodriguest@hotmail.com>
2023-01-06 17:58:34 -0500

    DeMorgans Law Solution

--- Fundamentals-8/n-divisible-by-xy/app.js
+++ Fundamentals-8/n-divisible-by-xy/app.js
@@ -16,3 +16,7 @@ const isDivisible = (n, x, y) => n % x == 0 && n % y == 0
 function isDivisible(n, x, y) {
   return !(n % x || n % y)
 }
+
+// DeMorgan's Law Solution
+
+const isDivisible = (n, x, y) => !((n % x) + (n % y))
