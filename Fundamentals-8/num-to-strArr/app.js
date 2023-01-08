1f6abff07aeb1d1c401ca1d6aee2828d5f1c7760
Allan <allanrodriguest@hotmail.com>
2023-01-08 18:35:47 -0500

    Better Performance Solution

--- Fundamentals-8/num-to-strArr/app.js
+++ Fundamentals-8/num-to-strArr/app.js
@@ -10,3 +10,16 @@
 function digitize(str) {
   return String(str).split('').reverse().map(Number)
 }
+
+// Improved Perfomance
+
+function digitize(n) {
+  n += ''
+  const result = []
+  for (let l = 0; l < Math.ceil(n.length / 2); l++) {
+    let r = n.length - 1 - l
+    result[l] = n.charAt(r) << 0
+    result[r] = n.charAt(l) << 0
+  }
+  return result
+}
