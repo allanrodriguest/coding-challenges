514bd9c6b6ab42dbea2100aeb1515c10022d54f6
Allan <allanrodriguest@hotmail.com>
2023-01-19 16:46:18 -0500

    Add Another Solution

--- Fundamentals-8/lover-petals/app.js
+++ Fundamentals-8/lover-petals/app.js
@@ -31,3 +31,17 @@ function howMuchILoveYou(nbPetals) {
   }
   return isLoved[nbPetals]
 }
+
+// Cleaner Solution
+
+const phrases = [
+  'I love you',
+  'a little',
+  'a lot',
+  'passionately',
+  'madly',
+  'not at all'
+]
+function howMuchILoveYou(n) {
+  return phrases[(n - 1) % phrases.length]
+}
