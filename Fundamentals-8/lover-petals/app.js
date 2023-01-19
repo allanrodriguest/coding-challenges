76deaef306bdc743ca644b66e478fa1509d9643b
Allan <allanrodriguest@hotmail.com>
2023-01-19 16:45:04 -0500

    Add Solution

--- Fundamentals-8/lover-petals/app.js
+++ Fundamentals-8/lover-petals/app.js
@@ -15,3 +15,19 @@
 // When the last petal was torn there were cries of excitement, dreams, surging thoughts and emotions.
 
 // Your goal in this kata is to determine which phrase the girls would say at the last petal for a flower of a given number of petals. The number of petals is always greater than 0.
+
+function howMuchILoveYou(nbPetals) {
+  let isLoved = {
+    1: 'I love you',
+    2: 'a little',
+    3: 'a lot',
+    4: 'passionately',
+    5: 'madly',
+    6: 'not at all'
+  }
+  if (nbPetals <= 6) return isLoved[nbPetals]
+  while (nbPetals > 6) {
+    nbPetals -= 6
+  }
+  return isLoved[nbPetals]
+}
