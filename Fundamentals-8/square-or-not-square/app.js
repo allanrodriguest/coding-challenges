02c8d55f616b7f36d0863b1772bfa0c75a5f9ae3
Allan <allanrodriguest@hotmail.com>
2023-01-20 17:14:21 -0500

    Add Solution

--- Fundamentals-8/square-or-not-square/app.js
+++ Fundamentals-8/square-or-not-square/app.js
@@ -12,3 +12,6 @@
 // [4,3,9,7,2,1] -> [2,9,3,49,4,1]
 // Notes
 // The input array will always contain only positive numbers, and will never be empty or null.
+
+const squareOrSquareRoot = arr =>
+  arr.map(item => (!(Math.sqrt(item) % 1) ? Math.sqrt(item) : item ** 2))
