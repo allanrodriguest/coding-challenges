d6453e88642178e7cb1ad42ac02d9d82b4248a9e
Allan <allanrodriguest@hotmail.com>
2023-01-22 18:12:14 -0500

    Add Solution

--- Fundamentals-8/infinity-loop/app.js
+++ Fundamentals-8/infinity-loop/app.js
@@ -4,3 +4,11 @@
 // DESCRIPTION:
 // Unfinished Loop - Bug Fixing #1
 // Oh no, Timmy's created an infinite loop! Help Timmy find and fix the bug in his unfinished for loop!
+
+const createArray = n => Array(...Array(n)).map((v, i) => i + 1)
+
+// Solution number 2
+
+function createArray(number) {
+  return Array.from({ length: number }, (v, i) => i + 1)
+}
