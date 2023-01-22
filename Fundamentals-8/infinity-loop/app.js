c1d05db6a16356e0692499e7dbee9ef18a17b6d8
Allan <allanrodriguest@hotmail.com>
2023-01-22 18:11:10 -0500

    Coding Challenge Jan 22

---
+++ Fundamentals-8/infinity-loop/app.js
@@ -0,0 +1,6 @@
+// 8 kyu
+// Unfinished Loop - Bug Fixing #1
+
+// DESCRIPTION:
+// Unfinished Loop - Bug Fixing #1
+// Oh no, Timmy's created an infinite loop! Help Timmy find and fix the bug in his unfinished for loop!
