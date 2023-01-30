057b56139794d802d4069f6d988105785090bc46
Allan <allanrodriguest@hotmail.com>
2023-01-29 19:57:12 -0500

    add solution

--- Fundamentals-8/uefa-euro-2016/app.js
+++ Fundamentals-8/uefa-euro-2016/app.js
@@ -6,3 +6,10 @@
 // uefaEuro2016(['Germany', 'Ukraine'],[2, 0]) // "At match Germany - Ukraine, Germany won!"
 // uefaEuro2016(['Belgium', 'Italy'],[0, 2]) // "At match Belgium - Italy, Italy won!"
 // uefaEuro2016(['Portugal', 'Iceland'],[1, 1]) // "At match Portugal - Iceland, teams played draw."
+
+const uefaEuro2016 = (teams, scores) =>
+  `At match ${teams[0]} - ${teams[1]}, ${
+    scores[0] == scores[1]
+      ? 'teams played draw.'
+      : `${scores[0] > scores[1] ? teams[0] : teams[1]} won!`
+  }`
