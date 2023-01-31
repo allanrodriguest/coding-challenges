2f69557e91eea78343ba614a858a2707fd835516
Allan <allanrodriguest@hotmail.com>
2023-01-31 16:24:46 -0500

    add solution

--- Fundamentals-8/duck-duck-goose/app.js
+++ Fundamentals-8/duck-duck-goose/app.js
@@ -10,3 +10,7 @@
 // duck_duck_goose([a, b, c, d], 1) should return a.name
 // duck_duck_goose([a, b, c, d], 5) should return a.name
 // duck_duck_goose([a, b, c, d], 4) should return d.name
+
+function duckDuckGoose(players, goose) {
+  return players[(goose - 1) % players.length].name
+}
