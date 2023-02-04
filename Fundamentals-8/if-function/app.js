02265603aa6a3e47ec306438a559b184d89618f0
Allan <allanrodriguest@hotmail.com>
2023-02-03 20:42:04 -0500

    add solution

--- Fundamentals-8/if-function/app.js
+++ Fundamentals-8/if-function/app.js
@@ -6,3 +6,5 @@
 // Example:
 // _if(true, function(){console.log("True")}, function(){console.log("false")})
 // // Logs 'True' to the console.
+
+const _if = (bool, func1, func2) => (bool ? func1() : func2())
