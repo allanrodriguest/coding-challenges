4280432d1e472ffc5c84c34a043680b4676e16f6
Allan <allanrodriguest@hotmail.com>
2023-02-07 19:41:19 -0500

    add solution

--- Fundamentals-8/oop-piracy/app.js
+++ Fundamentals-8/oop-piracy/app.js
@@ -25,3 +25,14 @@
 
 // titanic.isWorthIt() // return false
 // Good luck and may you find GOOOLD!
+
+class Ship {
+  constructor(draft, crew) {
+    this.draft = draft
+    this.crew = crew
+  }
+
+  isWorthIt() {
+    return this.draft - this.crew * 1.5 > 20
+  }
+}
