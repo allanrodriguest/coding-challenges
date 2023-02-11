73230d8c720ddc67204b5ad8087762ebd2cf8013
Allan <allanrodriguest@hotmail.com>
2023-02-10 20:06:07 -0500

    Coding Challenge Feb 10

---
+++ Fundamentals-8/validate-regex/app.js
@@ -0,0 +1,19 @@
+// DESCRIPTION:
+// Basic regex tasks. Write a function that takes in a numeric code of any length. The function should check if the code begins with 1, 2, or 3 and return true if so. Return false otherwise.
+
+// You can assume the input will always be a number.
+
+//Parameters: numeric number of any length
+
+//Results: Return true or false
+
+//Example: 10249 --> true
+//          5000 --> false
+
+//PSEUDOCODE:
+
+// Get the first digit of the numeric code
+// Check if it begins with 1, 2 or 3
+// return true or false
+
+
