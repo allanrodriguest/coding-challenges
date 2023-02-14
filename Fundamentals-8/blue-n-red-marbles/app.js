657124b91b8d1f583d9b99a222ad993f1bdf1520
Allan <allanrodriguest@hotmail.com>
2023-02-13 20:58:09 -0500

    add solution

--- Fundamentals-8/blue-n-red-marbles/app.js
+++ Fundamentals-8/blue-n-red-marbles/app.js
@@ -10,3 +10,6 @@
 // the number of blue marbles pulled out so far (always lower than the starting number of blue marbles)
 // the number of red marbles pulled out so far (always lower than the starting number of red marbles)
 // guessBlue() should return the probability of drawing a blue marble, expressed as a float. For example, guessBlue(5, 5, 2, 3) should return 0.6.
+
+const guessBlue = (blueStart, redStart, bluePulled, redPulled) =>
+  (blueStart - bluePulled) / (blueStart - bluePulled + redStart - redPulled)
