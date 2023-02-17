f17a57160b1992ab851f07e61e3af3882178fbc7
Allan <allanrodriguest@hotmail.com>
2023-02-16 21:25:38 -0500

    add solution

--- Fundamentals-8/chuck-norris/app.js
+++ Fundamentals-8/chuck-norris/app.js
@@ -5,3 +5,5 @@
 // Your task is to be more like Chuck (ha! good luck!). You must return false without ever actually using the word false...
 
 // Go show some truth who's boss!
+
+const ifChuckSaysSo = () => Boolean(0)
