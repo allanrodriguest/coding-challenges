58732ba7bc2a61d9e2f7fb2c381afa8ef01b8786
Allan <allanrodriguest@hotmail.com>
2023-02-23 23:19:56 -0500

    add solution

--- Fundamentals-8/five-trick/app.js
+++ Fundamentals-8/five-trick/app.js
@@ -6,3 +6,9 @@
 // multiply(200)==25000 // 200 * 5³
 // multiply(0)==0 // 0 * 5¹
 // multiply(-3)==-15 // -3 * 5¹
+
+function multiply(number) {
+  const n1 = '' + number,
+    FIVE = 5
+  return number * FIVE ** (n1[0] == '-' ? n1.slice(1).length : n1.length)
+}
