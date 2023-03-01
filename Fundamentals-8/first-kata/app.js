78e632ff015fd5b270af62d18f94de4394d23cf6
Allan <allanrodriguest@hotmail.com>
2023-02-28 22:25:13 -0500

    add solution

--- Fundamentals-8/first-kata/app.js
+++ Fundamentals-8/first-kata/app.js
@@ -10,3 +10,6 @@
 // You may assume the following:
 
 // If a and b are both numbers, neither of a or b will be 0.
+
+const myFirstKata = (a, b) =>
+  typeof a != 'number' || typeof b != 'number' ? false : (a % b) + (b % a)
