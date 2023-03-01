3dc1c1d3a4d5acf1e5f7133658d7f815cf0cf1d3
Allan <allanrodriguest@hotmail.com>
2023-02-28 22:21:43 -0500

    Coding Challenge Feb 28

---
+++ Fundamentals-8/first-kata/app.js
@@ -0,0 +1,12 @@
+// DESCRIPTION:
+// Fix the Bugs (Syntax) - My First Kata
+// Overview
+// Hello, this is my first Kata so forgive me if it is of poor quality.
+
+// In this Kata you should fix/create a program that returns the following values:
+
+// false/False if either a or b (or both) are not numbers
+// a % b plus b % a if both arguments are numbers
+// You may assume the following:
+
+// If a and b are both numbers, neither of a or b will be 0.
