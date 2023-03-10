427fe251efdf418dd71111594f6daad441691f1d
Allan <allanrodriguest@hotmail.com>
2023-03-10 11:20:15 -0500

    add solution

--- Fundamentals-8/obj-to-arr/app.js
+++ Fundamentals-8/obj-to-arr/app.js
@@ -1,2 +1,4 @@
 // DESCRIPTION:
 // You are trying to put a hash in ruby or an object in javascript or java into an array, but it always returns error, solve it and keep it as simple as possible!
+
+const items = new Array({ a: 'b', c: 'd' })
