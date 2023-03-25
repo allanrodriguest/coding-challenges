6564e7990fa8b946ccb16c616ba2225c778a7b0c
Allan <allanrodriguest@hotmail.com>
2023-03-24 21:49:38 -0400

    add solution

--- 7kyu/mumbling/app.js
+++ 7kyu/mumbling/app.js
@@ -6,3 +6,16 @@
 // accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
 // accum("cwAt") -> "C-Ww-Aaa-Tttt"
 // The parameter of accum is a string which includes only letters from a..z and A..Z.
+
+const accum = str =>
+  str
+    .split('')
+    .map(
+      (el, index) =>
+        `${
+          !index
+            ? el.toUpperCase()
+            : el.toUpperCase() + el.toLowerCase().repeat(index)
+        }`
+    )
+    .join('-')
